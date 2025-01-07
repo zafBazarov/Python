2d5e74ed63b1edd232cefe8970e4b6386b2b2a74
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-07 11:57:02 +0100

    Update Overview.py

--- Python For Data Visualization/Overview.py
+++ Python For Data Visualization/Overview.py
@@ -22,4 +22,34 @@
 # ***Data visualizations can be really powerful***
 
 
+# Leverage programming languages
+
+``There are a lot of great reasons to use a programming language like Python to build data visualizations. ``
+
+First, there are a lot of different types of visualizations you can make, including boxplots, bar graphs, line graphs, histograms, et cetera. 
+Second, using a programming language to build the data visualization allows you to quickly iterate and improve on a visualization, as well as try out many different visualizations. Why Python for data Visualization? 
+- Many types of visualizations
+  + Boxplots
+  + Bar graphs
+  + Line graphs
+  + Histograms
+- Codinga Data Visulization
+  + You to quickly iterate
+  + Imporove ona visulization
+  + Try out any different visualizations
+
+To get you started, I want you to first understand a data visualization process. You first have to have data, where you clean it, you simplify it, and sometimes even augment your data source if your data source is not rich enough. 
+From there, you can use that understanding from your visualization to improve your data quality or to explore other aspects of your data, and this process can keep on going until you have a visualization that tells a compelling story. 
+So there are a few common tools and libraries in Python to build data visualizations. In this course, we're going to utilize the Anaconda Python distribution, and second, I encourage you to use Jupyter Notebooks. 
+That's the program I'll be using throughout this course. Next, there are two major plotting libraries in Python, Matplotlib and Seaborn. And lastly, we'll go over the NumPy and Pandas libraries, as before you can visualize data, you have to get it, you have to clean it, and sometimes even augment it, and NumPy and Pandas help you do it. 
+So as far as why we use the Anaconda Python distribution, Python has a solid claim to be the fastest-growing major programming language, and Anaconda is the strongly recommended way of installing Jupyter Notebooks.
+So what Anaconda is really is a package manager, an environment manager, and Python distribution. Think of it as kind of like an app store for Python. Data visualization projects require many different packages and libraries. 
+What's great is that Anaconda comes pre-installed with many of them. So what about Jupyter? Jupyter Notebooks contain both code and rich text elements, such as visualizations, links, and equations. 
+So right here is a Jupyter Notebook in action, and you'll see a lot of that throughout this course. So I want to quickly tell you about NumPy and Pandas. 
+Before we can plot data, we first need data in a portable form. NumPy and Pandas can be used to efficiently load, store, manipulate, and export in memory data. 
+Pandas can also be used as a wrapper for Matplotlib, which brings us to Matplotlib and Seaborn. They're very popular Python plotting libraries. Matplotlib's API is relatively low-level. 
+This allows for a lot of customization, but also a lot of code. A way around this is to utilize a Seaborn wrapper, which offers high-level graphics and integrates well with Pandas' library. 
+It's also important to keep in mind that with every Matplotlib wrapper like Seaborn is still often useful to dive into Matplotlib's syntax to adjust the final plot output. And that's it. 
+These are a couple common tools you can use to build data visualizations.
+
 
