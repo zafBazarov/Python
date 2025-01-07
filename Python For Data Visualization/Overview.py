b48d17752c205f49df0891e7dd85cba713ac9a38
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-07 11:51:36 +0100

    Update Overview.py

--- Python For Data Visualization/Overview.py
+++ Python For Data Visualization/Overview.py
@@ -1,1 +1,25 @@
+## Data Visualization Overview
+
+
+# Value of data visualization
+
+# ***Data visualization*** is all about communicating results from data. 
+
+# It's about simplifying data, cleaning data with the end goal of helping you, and your stakeholders understand data on a more intuitive level. To bring this to life, I want to give you an example. 
+# Data visualization: 
+# - Simplifying Data
+# - Cleaning data
+# - Understanding data
+
+# Suppose you take out a loan, and you're given a payment table like this. When you look at this, what you really want to do is find out how much you're paying interest over time. 
+# picture
+
+#Now, this table is not really a great way to see this data. Instead, we might look to create a visualization like this. And as you can see, you actually pay more interest at the beginning of a loan, and this wouldn't have been clear if we didn't see this graph. 
+
+#Another way to look at this is by using a visualization like we see here. It's easy to see. The longer the term the loan, the more you end up paying an interest overall.
+
+# And just like that, data visualizations can be really powerful, and that's a reason why you should use visualizations in your day-to-day work.
+# ***Data visualizations can be really powerful***
+
+
 
