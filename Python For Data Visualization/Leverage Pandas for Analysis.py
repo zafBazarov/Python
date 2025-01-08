4c5a85a531883916474c1b0fa6113bacf3e04f4d
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-08 13:10:47 +0100

    Update Leverage Pandas for Analysis.py

--- Python For Data Visualization/Leverage Pandas for Analysis.py
+++ Python For Data Visualization/Leverage Pandas for Analysis.py
@@ -2,7 +2,7 @@
 
 # We will learn loading sample data, basic operations, data filtiring and so on using Pandas library. 
 
-# Load CSV File
+# Loading a dataset
 
 # Load car loan data from a scv file
 import pandas as pd
@@ -16,7 +16,9 @@ import pandas as pd
 df = pd.read_excel('car_financing.xlsx')
 print(df)
 
+# 
 
 # If we want to know more about these commands we can use help() function
 help(pd.read_excel)
-                 
+
+# There are many different file types you can load than just CSV and Excel files into Pandas DateFrames. If you're curious about what other kind of files you can load, I encourage you to do pd.read_ to see what other kind of files you can load. 
