48d63eca02e626e861d77d56e899b3fae479f3db
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-08 13:26:17 +0100

    Update Leverage Pandas for Analysis.py

--- Python For Data Visualization/Leverage Pandas for Analysis.py
+++ Python For Data Visualization/Leverage Pandas for Analysis.py
@@ -22,3 +22,26 @@ print(df)
 help(pd.read_excel)
 
 # There are many different file types you can load than just CSV and Excel files into Pandas DateFrames. If you're curious about what other kind of files you can load, I encourage you to do pd.read_ to see what other kind of files you can load. 
+
+## Basic Operations
+1. Assure that you have correctly loaded the data
+2. See what kind of data you have
+3. Check the validity of your data
+
+# Viewing the first and last 5 rows
+
+# Select top N number of records (default = 5)
+df.head()
+
+# Select bottom N number of records (default = 5)
+df.tail()
+
+# Check the column data types using the dtypes attribute
+df.dtypes
+
+# Use the shape attribute to get the number of rows and columns in your dataframe
+df.shape()
+
+# The infor method gives the column datatype + number of non-null values
+# Notice that we seem to have 405 non-null values for all but the Interest Paid column.
+df.info()
